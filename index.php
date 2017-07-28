0a0d4146280d2ee3c3a44af8bd9b0a67c1aafcd8
HecFranco <hector.franco.aceituno@gmail.com>
2017-07-28 12:42:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+require("core/Controller.php");
+require("core/Config.php");
+require("core/Views.php");
+require("core/Model.php");
+require("core/LoadModel.php");
+require("core/Layout.php");
+require("core/Functions.php");
+
+if(!session_start()){
+    session_start();
+}
+// Usuario Logueado ?
+// Está definida La variable / existe sesión?
+// Existe el Controlador ?
+if(isset($_SESSION["logged"])){
+    if(isset($_GET) && isset($_GET["controller"])){
+        $default_controller=$_GET["controller"];
+        // evitamos al volver hacia atrás podernos loguear de nuevo
+        if($_GET["controller"]=="Login" && $_GET["action"]=="index"){
+            $default_controller="Home";
+            $_GET["controller"]=$default_controller;
+        }
+        if(file_exists("controllers/".$_GET["controller"].".php")){
+            require("controllers/".$_GET["controller"].".php");       
+        }else{
+            die("controlador no encontrado ({$_GET["controller"]})");
+        }
+    }else{
+        $default_controller = "Home";
+        if(file_exists("controllers/".$default_controller.".php")){
+            require("controllers/".$default_controller.".php");       
+        }else{
+            die("controlador por defecto no encontrado ({$default_controller})");
+        }
+    }
+}else{
+    $default_controller ="Login"; 
+    if(file_exists("controllers/".$default_controller.".php")){
+        require("controllers/".$default_controller.".php");       
+    }else{
+        die("controlador por defecto no encontrado ({$default_controller})");
+    }
+}
+//Cargo el controlador por defecto
+$UsingSController=new $default_controller();
