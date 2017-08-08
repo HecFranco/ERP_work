98ce3fde1cff8a7c7abc8fc61eb359851fb4f9d9
HecFranco <hector.franco.aceituno@gmail.com>
2017-08-08 17:03:44 +0200

    Update Functions.php

--- core/Functions.php
+++ core/Functions.php
@@ -4,6 +4,7 @@ require("library/function.date.php");
 require("library/function.time.php");
 require("library/function.utilities.php");
 require("library/function.error-reporting.php");
+require("library/function.location.php");
 ?>
 
 
